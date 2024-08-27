b77f7e8d7e8eb484ca622dd8e0292672adf7e766
vinexfg <136178252+vinexfg@users.noreply.github.com>
2024-08-27 11:07:17 -0300

    a

---
+++ Verificador/main.py
@@ -0,0 +1,12 @@
+from verificador_de_senhas import Password
+
+print('Type your password')
+password_entered = input(': ')
+
+checker_password = Password()
+
+if checker_password.checker(password_entered):
+    if checker_password.checker_especial(password_entered):
+        print('Password valid')
+    else:
+        print('Password invalid')
---
+++ Verificador/verificador_de_senhas.py
@@ -0,0 +1,21 @@
+class Password:
+    def __init__(self):
+        self.caracteres_permitidos = 25
+        self.minimo_caracteres = 6
+        self.caracteres_necessarios = ['!', '@', '#', '%', '&', '*']
+        
+    def checker(self, password):
+        if len(password) > self.caracteres_permitidos:
+            print('Máximo de caracteres atingido')
+            return False
+        elif len(password) < self.minimo_caracteres:
+            print('A senha deve ter no mínimo 6 caracteres')
+            return False
+        else:
+            return True
+        
+    def checker_especial(self, password):
+        if not any(char in password for char in self.caracteres_necessarios):
+            print('A senha deve conter pelo menos um caractere especial: !, @, #, %, &, *')
+            return False
+        return True
